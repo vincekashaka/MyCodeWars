2e6f1811a9d1e487cc41527e35f421e59cdb413c
Vincent Kashaka <vincekashaka@gmail.com>
2022-05-25 23:08:33 +0200

    add the digitalRoot file

---
+++ 6th Kyu/diditalRoot.js
@@ -0,0 +1,9 @@
+/*
+Given n, take the sum of the digits of n. If that value 
+has more than one digit, continue reducing in this way until a 
+single-digit number is produced. The input will be a non-negative 
+integer.
+*/
+function digital_root(n){
+    return (n -1 ) % 9 + 1;
+}
